83348bc2a81c274591e0cdc8fc0422041dc80cf0
floatval <mmqchl@gmail.com>
2018-06-07 19:34:51 +0800

    安装脚本更新

--- install.sh
+++ install.sh
@@ -1,9 +1,82 @@
 #!/bin/bash
 
+# 依赖项自动安装
+## curl 安装
+### Centos
+echo -e "你使用的是 Centos 么？(y/n)"
+read answer
+if [ "$answer" = "y" ]
+  then
+    echo "请安装redhat-lsb"
+    sudo yum install redhat-lsb
+fi
+
+## curl 安装
+## 字体安装'
+exits_curl=$(which curl)
+if ($exits_curl)
+then
+  echo "你的系统已经安装过 curl 将自动进行下一步安装"
+else
+  echo "你的系统未安装 curl 下面将进行安装： curl"
+  sys=$( lsb_release -a | awk NR==3'{print $2,$3}' )
+  if [ "$sys" = "Arch Linux" ]
+    then
+      sudo pacman -S curl
+      echo -e "你是否已经安装过 power-line 字体？(y/n)"
+      read answer
+      if [ "$answer" = "n" ]
+        then
+          git clone https://github.com/powerline/fonts.git --depth=1
+          cd fonts
+          ./install.sh
+          rm -rf fonts
+      fi
+  elif [ "$sys" = "Ubuntu Linux" ]
+    then
+      sudo apt install curl
+      echo -e "你是否已经安装过 power-line 字体？(y/n)"
+      read answer
+      if [ "$answer" = "n" ]
+        then
+          sudo apt-get install fonts-powerline
+      fi
+  elif [ "$sys" = "CentOS Linux" ]
+    then
+       sudo yum install curl
+      echo -e "你是否已经安装过 power-line 字体？(y/n)"
+      read answer
+      if [ "$answer" = "n" ]
+        then
+          git clone https://github.com/powerline/fonts.git --depth=1
+          cd fonts
+          ./install.sh
+          rm -rf fonts
+      fi
+  fi
+fi
+
+
+# 根据是否已经开启 socks5 代理来自动配置git通过代理下载
+# 下载完成后自动重置代理
+
+# TODO
+
+# vim 安装目录配置
 vimHome="$HOME/.vim"
 if [ -e $vimHome ]
 then
-  git clone https://github.com/floatval/vimrc vimHome
+  echo "注意，下面的操作将会删除你的 $HOME/.vim 目录，请做好备份"
+  echo -e "请问你是否已完成备份？(y/n)"
+  echo "好啦，需要手动确认的已经全部完成了， 下面脚本会自动进行安装！ enjoy it！"
+  read answer
+  if [ "$answer" = "y" ]
+    then
+      rm -rf $HOME/.vim
+  else
+    exit
+  fi
+  git clone https://github.com/floatval/vimrc $vimHome
   curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 else
@@ -11,5 +84,7 @@ else
   git clone https://github.com/floatval/vimrc vimHome
   curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
 
-vim -c :PluginInstall
+# 插件安装:
+vim -c :PlugInstall
